9e669bbacd83e5991e6c1be6bdfc4b9dd3f12dcb
sundersk <125823301+sundersk@users.noreply.github.com>
2024-01-11 18:44:10 +0530

    Update opensession.sh

--- opensession.sh
+++ opensession.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-email_recipient="email@abc.com"
+email_recipient="your@email.com"
 current_date=$(date +"%Y-%m-%d")
-log_file="/root/$current_date.log"  # Adjust the path accordingly
+log_file="path/to/logs/$current_date.log"  # Adjust the path accordingly
 
 # Extract the numeric value of open sessions
 open_sessions=$(tac "$log_file" | grep -m 1 -o "Number of open sessions '[0-9]\+'" | awk -F"'" '{print $2}')
 
+# Get the server hostname
+server_hostname=$(hostname)
+
 # Check if the numeric value is greater than 100
 if [ "$open_sessions" -gt 100 ]; then
     # Send an email
-    echo "Subject: High Number of Open Sessions Alert" | mail -s "High Number of Open Sessions Alert" "$email_recipient"
+    email_subject="High Number of Open Sessions Alert on $server_hostname"
+    email_body="Server Hostname: $server_hostname\nDate: $current_date\nOpen Sessions: $open_sessions"
+
+    echo -e "Subject: $email_subject\n\n$email_body" | mail -s "$email_subject" "$email_recipient"
     echo "Email sent. Open sessions: $open_sessions"
 else
     echo "Number of open sessions is not greater than 100. No email sent."
